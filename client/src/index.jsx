047000e88c601074d2a9601d83c0f2d805494c2f
Kimo Silva <e.k.silva441@gmail.com>
2020-12-26 16:40:13 -0800

    Basic env/server setup

---
+++ client/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const Index = () => {
+  return (
+    <div className="heading">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<Index />, document.getElementById('app'));
