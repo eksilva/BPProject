485fc61572308ed0bfd03bbc2bd1a8d438998be5
Kimo Silva <e.k.silva441@gmail.com>
2021-01-03 14:56:53 -0800

    table updates

--- client/src/components/RiskTable.jsx
+++ client/src/components/RiskTable.jsx
@@ -23,48 +23,48 @@ const RiskTable = () => {
         <tr>
           <td>2</td>
           <td>70</td>
-          <td>20</td>
-          <td>0</td>
+          <td>15</td>
+          <td>15</td>
           <td>0</td>
           <td>0</td>
         </tr>
         <tr>
           <td>3</td>
           <td>60</td>
-          <td>20</td>
-          <td>0</td>
-          <td>0</td>
+          <td>15</td>
+          <td>15</td>
+          <td>10</td>
           <td>0</td>
         </tr>
         <tr>
           <td>4</td>
           <td>50</td>
           <td>20</td>
-          <td>0</td>
-          <td>0</td>
+          <td>20</td>
+          <td>10</td>
           <td>0</td>
         </tr>
         <tr>
           <td>5</td>
           <td>40</td>
           <td>20</td>
-          <td>0</td>
-          <td>0</td>
+          <td>20</td>
+          <td>20</td>
           <td>0</td>
         </tr>
         <tr>
           <td>6</td>
           <td>30</td>
-          <td>20</td>
-          <td>0</td>
+          <td>25</td>
+          <td>5</td>
           <td>0</td>
           <td>0</td>
         </tr>
         <tr>
           <td>7</td>
           <td>20</td>
-          <td>20</td>
-          <td>0</td>
+          <td>25</td>
+          <td>25</td>
           <td>0</td>
           <td>0</td>
         </tr>
@@ -72,23 +72,23 @@ const RiskTable = () => {
           <td>8</td>
           <td>10</td>
           <td>20</td>
-          <td>0</td>
+          <td>40</td>
           <td>0</td>
           <td>0</td>
         </tr>
         <tr>
           <td>9</td>
           <td>5</td>
-          <td>20</td>
-          <td>0</td>
+          <td>15</td>
+          <td>40</td>
           <td>0</td>
           <td>0</td>
         </tr>
         <tr>
+          <td>10</td>
           <td>0</td>
-          <td>80</td>
-          <td>20</td>
-          <td>0</td>
+          <td>5</td>
+          <td>25</td>
           <td>0</td>
           <td>0</td>
         </tr>
